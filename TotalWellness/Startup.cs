b356cef3f60ae17935d94bff3df18a6227067b53
Ashley Fellows <ashley.r.fellows@gmail.com>
2022-02-01 18:04:18 -0500

    project startup

---
+++ TotalWellness/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TotalWellness.Startup))]
+namespace TotalWellness
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
